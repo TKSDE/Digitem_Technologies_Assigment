945df7779f2813dc78256dd5a05cedf66b3929e9
TKSDE <tekchandkumarsharma@gmail.com>
2024-10-02 17:04:50 +0530

    update

--- fastapi-hello-world/app.py
+++ fastapi-hello-world/app.py
@@ -8,4 +8,5 @@ async def read_root():
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=5000)
+    uvicorn.run(app, host="0.0.0.0", port=5000, log_level="error")
+
